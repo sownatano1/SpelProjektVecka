42faed18dda378000d1bc6175960d577acb7f9f8
sownatano1 <jonathandahre@gmail.com>
2025-11-03 13:58:18 +0100

    Player Attack 1.0

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -7,8 +7,11 @@ public class PlayerAttack : MonoBehaviour
     public GameObject attackPrefab;
     public Transform firePos;
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     private float attackTime = 0f;
     private float attackCooldown = 0.4f;
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
     void Start()
@@ -19,10 +22,16 @@ public class PlayerAttack : MonoBehaviour
     void Update()
     {
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         if (Input.GetKeyDown(attackKey) && attackTime >= Time.time)
         {
             Instantiate(attackPrefab, firePos.position, firePos.rotation);
             attackTime = Time.time + attackCooldown;
+=======
+        if (Input.GetKeyDown(attackKey))
+        {
+            Instantiate(attackPrefab, firePos.position, firePos.rotation);
+>>>>>>> Stashed changes
 =======
         if (Input.GetKeyDown(attackKey))
         {
