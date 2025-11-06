5be32631a1c3403f515f4d60cff1b113e097eff6
sownatano1 <jonathandahre@gmail.com>
2025-11-06 13:20:16 +0100

    Water Splash

---
+++ Assets/Scripts/WaterSplash.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class WaterSplash : MonoBehaviour
+{
+    public AudioSource splashAudio;
+    public AudioSource leavingWater;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            splashAudio.Play();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            leavingWater.Play();
+        }
+    }
+}
